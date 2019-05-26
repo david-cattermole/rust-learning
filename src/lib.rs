1cf6fab3d1de59857a612b3aad57af0357f6a12b
David Cattermole <cattermole91@gmail.com>
2019-05-26 13:08:27 +0100

    Added Rust function ready to be called by Rust.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,22 @@
+extern crate libc;
+
 use std;
+use libc::c_int;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    
-   #[test]
-   fn it_works() {
-       assert_eq!(2 + 2, 4);
-   }
+ use super::*;
 
+#[test]
+fn it_works() {
+    assert_eq!(2 + 2, 4);
+}
+
+}
+
+// Output a function for usage in C.
+#[no_mangle]
+pub extern "C" fn my_exported_func(num: c_int) -> c_int {
+println!("Rust Number is {}", num);
+num + 1
 }
