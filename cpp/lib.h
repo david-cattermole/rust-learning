1b10808f73abf5c597ab06e6a7005d78ad573752
David Cattermole <cattermole91@gmail.com>
2019-05-26 13:07:48 +0100

    Added C++ test executable and header for using rust function.

---
+++ cpp/lib.h
@@ -0,0 +1,10 @@
+// Header file for C function usage.
+
+#ifndef LIB_H
+#define LIB_H
+
+extern "C" {
+     int my_exported_func(int num);
+}
+
+#endif // LIB_H
---
+++ cpp/main.cpp
@@ -0,0 +1,20 @@
+// Run a Rust function.
+#include <vector>
+#include <lib.h>
+
+void main() {
+     std::vector<int> my_vec;
+     my_vec.push_back(1);
+     my_vec.push_back(2);
+     my_vec.push_back(40);
+     my_vec.push_back(41);
+     my_vec.push_back(42);
+     my_vec.push_back(43);
+     std::vector<int>::iterator it;
+     for (it = my_vec.begin(); it != my_vec.end(); ++it) {
+          int c_num = *it;
+          int rust_num = my_exported_func(c_num);
+     }
+     my_exported_func(42);
+     return;
+}
